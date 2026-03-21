dc6f53e99cd2c3ed854e7bd943655893863cbcd4
vika <vikashinder@gmail.com>
2026-03-21 11:09:50 +0700

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
 import Mainnav from "./../ui/mainnav";
 import ContactForm from "./../ui/contactform";
 import RegisterForm from "./../ui/registerform";
 import NavigationPanel from "./../ui/navigationpanel";
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
 
 export default async function Home() {
   return (
